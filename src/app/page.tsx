1622b019ee40f7d8df8f907e6d8a6671c332feb9
mehedii-h0 <mehedii.h03@gmail.com>
2025-05-14 11:18:06 +0600

    background shape added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto px-6 xl:px-12">
+      {/* Background shape */}
+      <div
+        style={{ backgroundImage: "url(/shape-1.png)" }}
+        className="absolute inset-0 top-0 left-0 bg-cover bg-no-repeat bg-center -z-10"
+      />
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>Welcome to the Hero page</h1>
+    </div>
+  );
+};
+
+export default Hero;
